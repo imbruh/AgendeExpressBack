36a954dfe3c4ddb1e67c0a48ceb0780adc30c4bd
imbruh <brunaluiza112001@gmail.com>
2021-12-27 22:05:01 -0300

    backend da aplicaçao v2

--- src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Controller/HorarioController.java
+++ src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Controller/HorarioController.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,15 +22,16 @@ import br.edu.ifpb.agendeexpress.AgendeExpress.Service.HorarioService;
 
 @RestController
 @RequestMapping(value = "/horario")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://agende-express-front.herokuapp.com")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class HorarioController {
 
 	@Autowired
 	private HorarioService horarioService;
 	
 	@PostMapping("/cadastrar")
-	public void cadastrar(@RequestBody HorarioCadastrarDTO dto) {
-		this.horarioService.cadastrar(dto);
+	public Boolean cadastrar(@RequestBody HorarioCadastrarDTO dto) {
+		return this.horarioService.cadastrar(dto);
 	}
 	
 	@GetMapping("/listar")
@@ -41,4 +43,15 @@ public class HorarioController {
 	public List<String> filtrar(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime data, @RequestParam @NotNull Long idEmpresa){
 		return this.horarioService.filtrar(data, idEmpresa);
 	}
+	
+	@GetMapping("/buscar-horario-por-cliente")
+	public List<HorarioListarDTO> buscarHorarioPorCliente(@RequestParam @NotNull Long idCliente, @RequestParam @NotNull Long idEmpresa){
+		return this.horarioService.buscarHorarioPorCliente(idCliente, idEmpresa);
+	}
+	
+	@DeleteMapping("/apagar-horario")
+	public List<HorarioListarDTO> apagarHorario(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHora, @RequestParam Long idCliente, @RequestParam Long idEmpresa ) {
+		return this.horarioService.apagar(dataHora, idCliente, idEmpresa);
+	}
+	
 }
--- src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Service/HorarioService.java
+++ src/main/java/br/edu/ifpb/agendeexpress/AgendeExpress/Service/HorarioService.java
@@ -2,10 +2,13 @@ package br.edu.ifpb.agendeexpress.AgendeExpress.Service;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,57 +33,51 @@ public class HorarioService {
 	@Autowired
 	private EmpresaRepository empresaRepository;
 	
-	public void cadastrar(HorarioCadastrarDTO horario) {
-		Horario horarioExistente = horarioRepository.findByDatahora(horario.getDataHora());
-		Optional<Cliente> cliente = clienteRepository.findById(horario.getIdCliente());
-		Optional<Empresa> empresa = empresaRepository.findById(horario.getIdEmpresa());
-	
+	public Boolean cadastrar(HorarioCadastrarDTO horario) {
+		
+		Optional<Cliente> cliente = this.clienteRepository.findById(horario.getIdCliente());
+		Optional<Empresa> empresa = this.empresaRepository.findById(horario.getIdEmpresa());
+		List<Horario> horarios = this.horarioRepository.buscarHorariosPorClienteEmpresa(horario.getDataHora().getYear(), horario.getDataHora().getMonthValue(), horario.getDataHora().getDayOfMonth(), empresa.get(), cliente.get());
+		if (horarios.size()>=2) {
+			return false;
+		}
 		horarioRepository.save(Horario.builder()
 				.datahora(horario.getDataHora())
 				.cliente(cliente.get())
 				.empresa(empresa.get())
 				.build());
-				
+		return true;		
 	}
 	
 	public List<HorarioListarDTO> listar(LocalDateTime dataHora, Long idEmpresa){
-		List<Horario> horarios = horarioRepository.listarPorDia(dataHora, idEmpresa);
-		List<HorarioListarDTO> horariosDTO = new ArrayList<>();
+		Optional<Empresa> empresa = this.empresaRepository.findById(idEmpresa);
+		List<Horario> horarios = this.horarioRepository.listarPorDia(dataHora.getYear(),dataHora.getMonthValue(),dataHora.getDayOfMonth(), empresa.get());
 		
-		for(Horario hr : horarios) {
-			HorarioListarDTO hrDTO =  HorarioListarDTO.builder()
-			.ano(hr.getDatahora().getYear())
-			.mes(hr.getDatahora().getMonthValue())
-			.dia(hr.getDatahora().getDayOfMonth() < 10 ? "0" + hr.getDatahora().getDayOfMonth() : "" + hr.getDatahora().getDayOfMonth())
-			.hora(hr.getDatahora().getHour() < 10 ? "0" + hr.getDatahora().getHour() : "" + hr.getDatahora().getHour())
-			.minuto(hr.getDatahora().getMinute())
-			.diaSemana(hr.getDatahora().getDayOfWeek().getValue())
-			.build();
-			horariosDTO.add(hrDTO);
-		}
-		
-		return horariosDTO;
-
+		return this.convertDTO(horarios);
 	}
 
 	public List<String> filtrar(LocalDateTime data , Long idEmpresa) {
 		LocalDateTime horaHoje = LocalDateTime.now();
+		Optional<Empresa> empresa = this.empresaRepository.findById(idEmpresa);
+		List<String> horas = new ArrayList<>();
 		
-		List<Horario> horarios = this.horarioRepository.listarPorDia(data, idEmpresa);
+		if (data.isBefore(horaHoje)) {
+			return horas;
+		}
+		
+		List<Horario> horarios = this.horarioRepository.listarPorDia(data.getYear(),data.getMonthValue(),data.getDayOfMonth(), empresa.get());
 		
 		List<String> horariosMarcados = new ArrayList<>();
 		for(Horario h: horarios){
 			horariosMarcados.add(Integer.toString(h.getDatahora().getHour()));
 		}
 		
-		List<String> horas = new ArrayList<>();
-		System.out.println(data.toLocalDate());
-		System.out.println(horaHoje.toLocalDate());
 		if(data.toLocalDate().equals(horaHoje.toLocalDate())) {
 			for(int i = 8; i < 18; i++) {
 				if(!horariosMarcados.contains(Integer.toString(i))){
 					if(i > horaHoje.getHour())
 						horas.add(Integer.toString(i));	
+					
 				}
 			}
 		}
@@ -92,5 +89,48 @@ public class HorarioService {
 			}
 		}
 		return horas;
+	}
+
+	public List<HorarioListarDTO> apagar(LocalDateTime dataHora, Long idCliente, Long idEmpresa){
+		Optional<Cliente> cliente = this.clienteRepository.findById(idCliente);
+		Optional<Empresa> empresa = this.empresaRepository.findById(idEmpresa);
+		Horario horario = this.horarioRepository.findByDatahoraAndClienteAndEmpresa(dataHora, cliente.get(), empresa.get());
+		if (horario == null) {
+			return null;
+		}
+		this.horarioRepository.delete(horario);
+		
+		return this.buscarHorarioPorCliente(idCliente, idEmpresa);
+	}
+
+	public List<HorarioListarDTO> buscarHorarioPorCliente(Long idCliente, Long idEmpresa) {
+		Optional<Cliente> cliente = this.clienteRepository.findById(idCliente);
+		Optional<Empresa> empresa = this.empresaRepository.findById(idEmpresa);
+		
+		List<Horario> horarios = new ArrayList<>();
+		if(cliente.isPresent() && empresa.isPresent()) {
+			horarios = this.horarioRepository.findByClienteAndEmpresa(cliente.get(), empresa.get()); 
+		}
+		
+		return this.convertDTO(horarios);
+	}
+	
+	private List<HorarioListarDTO> convertDTO (List<Horario> horarios) {
+		List<HorarioListarDTO> horariosDTO = new ArrayList<>();
+		
+		for(Horario hr : horarios) {
+			HorarioListarDTO hrDTO =  HorarioListarDTO.builder()
+			.ano(hr.getDatahora().getYear())
+			.mes(hr.getDatahora().getMonthValue())
+			.dia(hr.getDatahora().getDayOfMonth() < 10 ? "0" + hr.getDatahora().getDayOfMonth() : "" + hr.getDatahora().getDayOfMonth())
+			.hora(hr.getDatahora().getHour() < 10 ? "0" + hr.getDatahora().getHour() : "" + hr.getDatahora().getHour())
+			.minuto(hr.getDatahora().getMinute())
+			.diaSemana(hr.getDatahora().getDayOfWeek().getValue())
+			.build();
+			
+			horariosDTO.add(hrDTO);
+		}
+		
+		return horariosDTO;
 	}
 }
